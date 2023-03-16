adc2473e4dd13f8c9b8d36af5c4eed32f93cdf44
Keith Bennett <keith@airmap.com>
2023-03-16 15:44:31 -0500

    add Case 6

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -xeuo pipefail
+
+# Errors are expected in this script.
+set +e
+
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 GIT_SHA="$(git rev-parse HEAD)"
 export GIT_SHA
@@ -22,8 +27,9 @@ echo "
 "
 export DOCKER_BUILDKIT=0
 remove_cache
-DOCKER_BUILDKIT=0 docker build --build-arg GIT_SHA="${GIT_SHA}" --tag "insanity_a:${GIT_SHA}" a/
-DOCKER_BUILDKIT=0 docker build --build-arg INSANITY_A_VERSION="${GIT_SHA}" --tag "insanity_b:${GIT_SHA}" b/
+docker build --build-arg GIT_SHA="${GIT_SHA}" --tag "insanity_a:${GIT_SHA}" a/
+docker images insanity_a
+docker build --build-arg INSANITY_A_VERSION="${GIT_SHA}" --tag "insanity_b:${GIT_SHA}" b/
 
 echo "
 ################################################################################
@@ -37,6 +43,7 @@ echo "
 export DOCKER_BUILDKIT=0
 remove_cache
 docker compose -f ./docker-compose.yml build insanity_a
+docker images insanity_a
 docker compose -f ./docker-compose.yml build insanity_b
 
 echo "
@@ -49,6 +56,7 @@ echo "
 export DOCKER_BUILDKIT=1
 remove_cache
 docker build --build-arg GIT_SHA="${GIT_SHA}" --tag "insanity_a:${GIT_SHA}" a/
+docker images insanity_a
 docker build --build-arg INSANITY_A_VERSION="${GIT_SHA}" --tag "insanity_b:${GIT_SHA}" b/
 
 echo "
@@ -63,6 +71,7 @@ echo "
 export DOCKER_BUILDKIT=1
 remove_cache
 docker compose -f ./docker-compose.yml build insanity_a
+docker images insanity_a
 docker compose -f ./docker-compose.yml build insanity_b
 
 echo "
@@ -77,7 +86,21 @@ echo "
 unset DOCKER_BUILDKIT
 remove_cache
 docker buildx build --load --build-arg GIT_SHA="${GIT_SHA}" --tag "insanity_a:${GIT_SHA}" a/
+docker images insanity_a
 docker buildx build --load --build-arg INSANITY_A_VERSION="${GIT_SHA}" --tag "insanity_b:${GIT_SHA}" b/
+
+echo '
+################################################################################
+################################################################################
+# Case 6: \`docker buildx build\` (REQUIREs buildkit) using `--cache-from`
+################################################################################
+################################################################################
+'
+unset DOCKER_BUILDKIT
+remove_cache
+docker buildx build --load --build-arg GIT_SHA="${GIT_SHA}" --tag "insanity_a:${GIT_SHA}" a/
+docker images insanity_a
+docker buildx build --load --build-arg INSANITY_A_VERSION="${GIT_SHA}" "--cache-from=insanity_a:${GIT_SHA}" --tag "insanity_b:${GIT_SHA}" b/
 
 # Remove cache for next test
 remove_cache
